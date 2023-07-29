d0f78875ff4c6aff71a2176186dcf2a747e119d0
Marshmalow212 <erfanultaher@yahoo.com>
2023-07-30 02:10:47 +0600

    themes content added

---
+++ wp-content/themes/twentytwentyone/custom-template.php
@@ -0,0 +1,17 @@
+<?php 
+/* Template Name: Custom Template */
+get_header();
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <label id='testlabel' >nothing</label>
+            
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
